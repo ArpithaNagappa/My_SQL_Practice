4dfebe58a4165aa2a5d535d20029fa903babffc9
Arpitha Nagappa <arpithagn021@gmail.com>
2026-02-16 23:13:05 -0600

    Day2 Practice

---
+++ Day 2.sql
@@ -0,0 +1,11 @@
+Select * from logins
+
+select * from sessions
+
+select * from employees where dept IN ('HR','SALES')
+
+select * from users
+
+select * from users where city like 'S%'
+
+Select Upper(name) from users where Upper(name) like '%C%'
